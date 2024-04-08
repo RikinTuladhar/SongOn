4d67b679b30b12e41268e331770fcb2bae8b64c0
Rikin Tuladhar <110732947+Senyadt2@users.noreply.github.com>
2024-04-09 02:01:00 +0545

    Notify

--- fontend/src/pages/HomePage.jsx
+++ fontend/src/pages/HomePage.jsx
@@ -1,26 +1,39 @@
-import React, { useEffect } from 'react'
-import SongPlayer from '../components/SongPlayer'
-import HomeLeft from '../components/HomeLeft'
-import HomeRight from '../components/HomeRight'
-
+import React, { useEffect } from "react";
+import SongPlayer from "../components/SongPlayer";
+import HomeLeft from "../components/HomeLeft";
+import HomeRight from "../components/HomeRight";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const HomePage = () => {
+  useEffect(() => {
 
-    useEffect(()=>{
-
-    },[])
+    toast.info("Click on song and press next song from panel to listen!!");
+  }, []);
 
   return (
-    <div className='w-full h-[auto] min-h-[100vh] bg-[#000000] relative  mt-[10vh] pt-5 md:pb-40 md:mt-[10vh] overflow-hidden flex flex-col items-center gap-3'>
-       <h1 className='text-2xl my-3  md:my-0 md:text-3xl font-extrabold text-center text-[#E5E7EB] '>Welcome to my songs list</h1>
-       <div className="container flex flex-wrap w-full h-full gap-5 md:flex-nowrap">
-        <HomeLeft/>
-        <HomeRight/>
-       </div>
-      
-
+    <div className="w-full h-[auto] min-h-[100vh] bg-[#000000] relative  mt-[10vh] pt-5 md:pb-40 md:mt-[10vh] overflow-hidden flex flex-col items-center gap-3">
+      <ToastContainer
+        position="top-center"
+        autoClose={false}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+      <h1 className="text-2xl my-3  md:my-0 md:text-3xl font-extrabold text-center text-[#E5E7EB] ">
+        Welcome to my songs list
+      </h1>
+      <div className="container flex flex-wrap w-full h-full gap-5 md:flex-nowrap">
+        <HomeLeft />
+        <HomeRight />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
