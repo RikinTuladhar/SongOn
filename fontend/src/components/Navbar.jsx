6eeb1c0fb6e8cb301aa06157c1404891517b462d
Rikin Tuladhar <110732947+RikinTuladhar@users.noreply.github.com>
2024-05-16 16:32:16 +0545

    Update Navbar.jsx

--- fontend/src/components/Navbar.jsx
+++ fontend/src/components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
           <Link to={"/addGenre"} class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Add Generic</Link>
         </li>
         <li>
-          <Link to={"/Logout"} class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Logout</Link>
+          <Link to={"/admin"} class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Admin</Link>
         </li>
       </ul>
     </div>
