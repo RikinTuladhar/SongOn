2126f21999a1ba19289f61f229a19170665a8acf
Rikin Tuladhar <110732947+RikinTuladhar@users.noreply.github.com>
2024-06-05 15:47:15 +0545

    small update on UI

--- fontend/src/components/Navbar.jsx
+++ fontend/src/components/Navbar.jsx
@@ -142,14 +142,14 @@ const Navbar = () => {
                         localStorage.removeItem("token");
                         dispatch(logOut());
                       }}
-                      className="block px-3 py-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                      className="block px-3 py-2 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                     >
                       Sign Out
                     </Link>
                   ) : (
                     <Link
                       to={"/signin"}
-                      className="block px-3 py-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                      className="block px-3 py-2 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                     >
                       Sign in
                     </Link>
