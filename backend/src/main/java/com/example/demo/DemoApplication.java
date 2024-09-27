a51e88c24b95ea20dd1808d9de2191cfde32e38c
Rikin07 <110732947+RikinTuladhar@users.noreply.github.com>
2024-09-27 19:06:01 +0545

    update

--- backend/src/main/java/com/example/demo/DemoApplication.java
+++ backend/src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
