06c92fb68e6a59ebd56d404cfd66647126e65c35
Rikin Tuladhar <110732947+RikinTuladhar@users.noreply.github.com>
2024-10-03 08:05:01 +0545

    update

---
+++ musiconintro/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Spline from "@splinetool/react-spline/next";
+export default function Home() {
+  return (
+    <div className="w-full h-screen bg-slate-400">
+      
+      <Spline scene="https://prod.spline.design/EkLZ8Z13hqtkLYlL/scene.splinecode" />
+    </div>
+  );
+}
